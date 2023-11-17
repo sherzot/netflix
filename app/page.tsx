175ca97643b1988f2b695a2873cdc75bacdc85d4
Sherzod Musurmonov <sherzoddeveloper@gmail.com>
2023-11-17 12:17:06 +0900

    Netlifix clone version

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+// @flow strict
+
+import * as React from 'react';
+import { Button } from '@/components/ui/button';
+
+function Page() {
+  return (
+    <div>
+      <Button>Click</Button>
+    </div>
+  );
+};
+
+export default Page;
