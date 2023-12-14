6a8fb4cfb376c753e24deb5a44ed149cc2c1b786
sherzot <102805587+sherzot@users.noreply.github.com>
2023-12-14 10:28:44 +0900

    Add bg image

--- components/shared/login.tsx
+++ components/shared/login.tsx
@@ -1,10 +1,17 @@
 "use client"
 
 import * as React from 'react';
+import Image  from 'next/image';
 
 const Login = () => {
     return (
-        <div>
+        <div className={"w-full h-screen"}>
+            <div className={"absolute inset-0"}>
+               <Image src = {"https://repository-images.githubusercontent.com/299409710/b42f7780-0fe1-11eb-8460-e459acd20fb4"} 
+               alt = {"bg"} 
+               fill
+               />
+            </div>
             Login Component
         </div>
     );
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {protocol: 'https', hostname: 'x'},
+            {protocol: 'http', hostname: 'x'}
+        ],
+    }
+}
 
 module.exports = nextConfig
