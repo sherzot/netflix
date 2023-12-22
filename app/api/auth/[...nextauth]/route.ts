ddaa7f0149b5ae482f040aa0b4b3ba7c6849e1b8
sherzot <102805587+sherzot@users.noreply.github.com>
2023-12-22 09:45:50 +0900

    .

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,6 +7,7 @@ const authOptions: NextAuthOptions = {
             clientId: process.env.GITHUB_CLIENT_ID as string,
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string
         })
+
     ],
     callbacks: {
         async session({session, token}: any){
